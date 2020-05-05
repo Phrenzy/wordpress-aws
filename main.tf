f7e16a0a8aa4b9738407f07e2805fa8ba8b825c6
Dave Ballard <dave@phrenzy.com>
2020-05-05 15:11:12 -0700

    added subnets

--- main.tf
+++ main.tf
@@ -22,12 +22,85 @@ resource "aws_vpc" "wp_vpc" {
   }
 }
 
-# Internet gateway
+#### Internet gateway ####
 
 resource "aws_internet_gateway" "wp_internet_gateway" {
   vpc_id = aws_vpc.wp_vpc.id
 
   tags = {
     Name = "${var.stackname}_IGW"
+  }
+}
+
+#### Route tables ####
+
+resource "aws_route_table" "wp_public_rt" {
+  vpc_id = "${aws_vpc.wp_vpc.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.wp_internet_gateway.id}"
+  }
+
+  tags = {
+    Name = "${var.stackname}_public"
+  }
+}
+
+####  Subnets  ####
+
+resource "aws_subnet" "wp_public1_subnet" {
+  vpc_id                  = "${aws_vpc.wp_vpc.id}"
+  cidr_block              = "${var.cidrs["public1"]}"
+  map_public_ip_on_launch = true
+  availability_zone       = "${data.aws_availability_zones.available.names[0]}"
+
+  tags = {
+    Name = "${var.stackname}_public1"
+  }
+}
+
+resource "aws_subnet" "wp_public2_subnet" {
+  vpc_id                  = "${aws_vpc.wp_vpc.id}"
+  cidr_block              = "${var.cidrs["public2"]}"
+  map_public_ip_on_launch = true
+  availability_zone       = "${data.aws_availability_zones.available.names[1]}"
+
+  tags = {
+    Name = "${var.stackname}_public2"
+  }
+}
+
+
+resource "aws_subnet" "wp_rds1_subnet" {
+  vpc_id                  = "${aws_vpc.wp_vpc.id}"
+  cidr_block              = "${var.cidrs["rds1"]}"
+  map_public_ip_on_launch = false
+  availability_zone       = "${data.aws_availability_zones.available.names[0]}"
+
+  tags = {
+    Name = "${var.stackname}_rds1"
+  }
+}
+
+resource "aws_subnet" "wp_rds2_subnet" {
+  vpc_id                  = "${aws_vpc.wp_vpc.id}"
+  cidr_block              = "${var.cidrs["rds2"]}"
+  map_public_ip_on_launch = false
+  availability_zone       = "${data.aws_availability_zones.available.names[1]}"
+
+  tags = {
+    Name = "${var.stackname}_rds2"
+  }
+}
+
+resource "aws_subnet" "wp_rds3_subnet" {
+  vpc_id                  = "${aws_vpc.wp_vpc.id}"
+  cidr_block              = "${var.cidrs["rds3"]}"
+  map_public_ip_on_launch = false
+  availability_zone       = "${data.aws_availability_zones.available.names[2]}"
+
+  tags = {
+    Name = "${var.stackname}_rds3"
   }
 }
