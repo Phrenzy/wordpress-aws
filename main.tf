f3aef20f5d7f709fd104b1dffad6154192e2e4a0
Dave Ballard <dave@phrenzy.com>
2020-05-05 13:30:11 -0700

    first success

--- main.tf
+++ main.tf
@@ -1,2 +1,23 @@
 #
 # Written for Terraform v0.12.24
+
+provider "aws" {
+  region  = var.aws_region
+  profile = var.aws_profile
+}
+
+
+
+
+
+#-------- VPC --------
+
+resource "aws_vpc" "wp_vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "wp_vpc"
+  }
+}
